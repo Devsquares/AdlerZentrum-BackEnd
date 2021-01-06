42a86b04bffbc267f224db4c79239cf6d290c082
Gab <amrjablla@gmail.com>
2021-01-06 15:27:00 +0200

    handle login error

--- Application/DTOs/AccountDTO/RegisterRequest.cs
+++ Application/DTOs/AccountDTO/RegisterRequest.cs
@@ -9,26 +9,12 @@ using System.Text.Json.Serialization;
 namespace Application.DTOs.Account
 {
     public class RegisterRequest
-    {
-        [Required]
+    { 
         public string FirstName { get; set; }
-
-        [Required]
         public string LastName { get; set; }
-
-        [Required]
-        [EmailAddress]
         public string Email { get; set; }
-        [Required]
-        [MinLength(6)]
         public string UserName { get; set; }
-
-        [Required]
-        [MinLength(6)]
         public string Password { get; set; }
-
-        [Required]
-        [Compare("Password")]
         public string ConfirmPassword { get; set; }  
         public string Country { get; set; }
         public DateTime DateOfBirth { get; set; }
