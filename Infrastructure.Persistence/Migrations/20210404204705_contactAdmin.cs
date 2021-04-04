827976d466ed1271433ab98c8e42520f49eb3870
Gab <amrjablla@gmail.com>
2021-04-04 22:49:04 +0200

    body request

---
+++ Infrastructure.Persistence/Migrations/20210404204705_contactAdmin.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Persistence.Migrations
+{
+    public partial class contactAdmin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "MailJobs",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Message",
+                table: "MailJobs",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "MailJobs");
+
+            migrationBuilder.DropColumn(
+                name: "Message",
+                table: "MailJobs");
+        }
+    }
+}
