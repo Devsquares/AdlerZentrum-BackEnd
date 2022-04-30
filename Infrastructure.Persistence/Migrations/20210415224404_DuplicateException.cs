1b04c355df41aa721008553d048fc6e5c7bf4a1c
AmrGablla <amrjablla@gmail.com>
2022-05-01 00:19:19 +0200

    remove comment

--- Infrastructure.Persistence/Migrations/20210415224404_DuplicateException.cs
+++ Infrastructure.Persistence/Migrations/20210415224404_DuplicateException.cs
@@ -8,92 +8,92 @@ namespace Infrastructure.Persistence.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            // migrationBuilder.DropForeignKey(
-            //     name: "FK_teacherAbsences_LessonInstances_LessonInstanceId",
-            //     table: "teacherAbsences");
+            migrationBuilder.DropForeignKey(
+                name: "FK_teacherAbsences_LessonInstances_LessonInstanceId",
+                table: "teacherAbsences");
 
-            // migrationBuilder.DropForeignKey(
-            //     name: "FK_teacherAbsences_ApplicationUsers_TeacherId",
-            //     table: "teacherAbsences");
+            migrationBuilder.DropForeignKey(
+                name: "FK_teacherAbsences_ApplicationUsers_TeacherId",
+                table: "teacherAbsences");
 
-            // migrationBuilder.DropPrimaryKey(
-            //     name: "PK_teacherAbsences",
-            //     table: "teacherAbsences");
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_teacherAbsences",
+                table: "teacherAbsences");
 
-            // migrationBuilder.RenameTable(
-            //     name: "teacherAbsences",
-            //     newName: "TeacherAbsences");
+            migrationBuilder.RenameTable(
+                name: "teacherAbsences",
+                newName: "TeacherAbsences");
 
-            // migrationBuilder.RenameIndex(
-            //     name: "IX_teacherAbsences_TeacherId",
-            //     table: "TeacherAbsences",
-            //     newName: "IX_TeacherAbsences_TeacherId");
+            migrationBuilder.RenameIndex(
+                name: "IX_teacherAbsences_TeacherId",
+                table: "TeacherAbsences",
+                newName: "IX_TeacherAbsences_TeacherId");
 
-            // migrationBuilder.RenameIndex(
-            //     name: "IX_teacherAbsences_LessonInstanceId",
-            //     table: "TeacherAbsences",
-            //     newName: "IX_TeacherAbsences_LessonInstanceId");
+            migrationBuilder.RenameIndex(
+                name: "IX_teacherAbsences_LessonInstanceId",
+                table: "TeacherAbsences",
+                newName: "IX_TeacherAbsences_LessonInstanceId");
 
-            // migrationBuilder.AddColumn<bool>(
-            //     name: "DelaySeen",
-            //     table: "TestInstances",
-            //     nullable: false,
-            //     defaultValue: false);
+            migrationBuilder.AddColumn<bool>(
+                name: "DelaySeen",
+                table: "TestInstances",
+                nullable: false,
+                defaultValue: false);
 
-            // migrationBuilder.AddColumn<bool>(
-            //     name: "DelaySeen",
-            //     table: "LessonInstances",
-            //     nullable: false,
-            //     defaultValue: false);
+            migrationBuilder.AddColumn<bool>(
+                name: "DelaySeen",
+                table: "LessonInstances",
+                nullable: false,
+                defaultValue: false);
 
-            // migrationBuilder.AddColumn<DateTime>(
-            //     name: "DueDate",
-            //     table: "LessonInstances",
-            //     nullable: true);
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DueDate",
+                table: "LessonInstances",
+                nullable: true);
 
-            // migrationBuilder.AddColumn<bool>(
-            //     name: "DelaySeen",
-            //     table: "HomeWorkSubmitions",
-            //     nullable: false,
-            //     defaultValue: false);
+            migrationBuilder.AddColumn<bool>(
+                name: "DelaySeen",
+                table: "HomeWorkSubmitions",
+                nullable: false,
+                defaultValue: false);
 
-            // migrationBuilder.AddPrimaryKey(
-            //     name: "PK_TeacherAbsences",
-            //     table: "TeacherAbsences",
-            //     column: "Id");
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TeacherAbsences",
+                table: "TeacherAbsences",
+                column: "Id");
 
-            // migrationBuilder.CreateTable(
-            //     name: "DuplicateExceptions",
-            //     columns: table => new
-            //     {
-            //         Id = table.Column<int>(nullable: false)
-            //             .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
-            //         CreatedBy = table.Column<string>(maxLength: 256, nullable: true),
-            //         CreatedDate = table.Column<DateTime>(nullable: true),
-            //         LastModifiedBy = table.Column<string>(maxLength: 256, nullable: true),
-            //         LastModifiedDate = table.Column<DateTime>(nullable: true),
-            //         Email = table.Column<string>(nullable: true)
-            //     },
-            //     constraints: table =>
-            //     {
-            //         table.PrimaryKey("PK_DuplicateExceptions", x => x.Id);
-            //     });
+            migrationBuilder.CreateTable(
+                name: "DuplicateExceptions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    CreatedBy = table.Column<string>(maxLength: 256, nullable: true),
+                    CreatedDate = table.Column<DateTime>(nullable: true),
+                    LastModifiedBy = table.Column<string>(maxLength: 256, nullable: true),
+                    LastModifiedDate = table.Column<DateTime>(nullable: true),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DuplicateExceptions", x => x.Id);
+                });
 
-            // migrationBuilder.AddForeignKey(
-            //     name: "FK_TeacherAbsences_LessonInstances_LessonInstanceId",
-            //     table: "TeacherAbsences",
-            //     column: "LessonInstanceId",
-            //     principalTable: "LessonInstances",
-            //     principalColumn: "Id",
-            //     onDelete: ReferentialAction.Cascade);
+            migrationBuilder.AddForeignKey(
+                name: "FK_TeacherAbsences_LessonInstances_LessonInstanceId",
+                table: "TeacherAbsences",
+                column: "LessonInstanceId",
+                principalTable: "LessonInstances",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
 
-            // migrationBuilder.AddForeignKey(
-            //     name: "FK_TeacherAbsences_ApplicationUsers_TeacherId",
-            //     table: "TeacherAbsences",
-            //     column: "TeacherId",
-            //     principalTable: "ApplicationUsers",
-            //     principalColumn: "Id",
-            //     onDelete: ReferentialAction.Restrict);
+            migrationBuilder.AddForeignKey(
+                name: "FK_TeacherAbsences_ApplicationUsers_TeacherId",
+                table: "TeacherAbsences",
+                column: "TeacherId",
+                principalTable: "ApplicationUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
