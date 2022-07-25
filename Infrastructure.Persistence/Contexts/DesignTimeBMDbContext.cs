ec00d77be8e5a4e2b5cd374421dfc97162602fc5
Amr GabAllah <amr.gaballah@rology.net>
2022-07-26 00:13:56 +0200

    fix issue

---
+++ Infrastructure.Persistence/Contexts/DesignTimeBMDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Infrastructure.Persistence
+{
+    public class DesignTimeBMDbContext : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>(); 
+            optionsBuilder.UseMySql(configuration.GetConnectionString("DefaultConnection"));
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
